043d1606bdcbcf2ec07acd3002228b2991a37399
JimmyOjeda <ojedajimmy1@gmail.com>
2022-07-27 18:45:07 -0500

    block_promuser fixed

--- block_promuser.php
+++ block_promuser.php
@@ -17,19 +17,21 @@ class block_promuser extends block_base {
         global $DB;
         global $COURSE;
         global $USER;
-        $this->content = new stdClass;
         $courseId = $COURSE->id;
 
 
-        //Si ya existe el contenido, volverlo a mostrar
+        //Si ya existe el contenido lo vuelve a mostrar, sino, crea nuevo contenido
         if ($this->content !== null) {
             return $this->content;
+        }else{
+            $this->content = new stdClass;
         }
         //--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
         //Se incluyen las vistas tipo "modal"
         include('views/activitiesByInterval.php');
         include('views/promTimeByUser.php');
+        include('main/main.php');
         //--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -73,74 +75,7 @@ class block_promuser extends block_base {
         //--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-        //Se declara el código HTML del bloque PromUser
-        $contentPromUser = <<< EOT
-            <button onclick="showInformation()" id="buttonShowInformation" style="width: 100%;" class="btn btn-sm btn-warning">
-                Obtener tiempos promedio
-            </button>
-
-            <br>
-            <span style='color:blue'>
-                El tiempo promedio que estuvo el grupo en las actividades del curso en una sesión es de: <strong id='generalInformation1'>-:-:-</strong> hrs/min/seg.
-            </span>
-            <br>
-
-            <br>
-            <span style='color: green'>
-                El tiempo promedio que estuvo el grupo en las actividades del curso por día es de: <strong id='generalInformation2'>-:-:-</strong> hrs/min/seg.
-            </span>
-            <hr>
-
-            <h6>Tiempo promedio del grupo por actividad</h6>
-
-            <div id='buttonActivitiesPerInterval'>
-                <button id="modalButton1" onclick="showTableActivitiesPerInterval()" style="width: 100%;" type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#exampleModal">
-                    Ver detalle
-                </button>
-            </div>
-            
-            <div id='buttonCSV'></div>
-            <div id='buttonCSVInterval'></div>
-            <hr>
-
-            <div id='buttonActivitiesPerInterval'></div>
-
-            <h6>Tiempo promedio del grupo por día</h6>
-
-            <div id='buttonActivitiesPerDay'>
-                <button id="modalButton2" onclick="showTableActivitiesPerDay()" style="width: 100%;" type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#exampleModal"> 
-                    Ver detalle
-                </button>
-            </div>
-
-            <div id='graphic-class' class='btn btn-sm btn-primary mt-1' onclick='showGraphicClassNetworks()'>
-                Mostrar gráfico
-            </div>
-            <hr>
-
-            <h6>Seleccione un usuario para desplegar sus estadísticas de acceso:</h6>
-            
-            <select style="max-width: 100%; min-width: 100%;" id="selectUserId">
-            
-            $selectOptions
-
-            </select>
-            <div style='margin-top:10px;' id='buttonPromByUser'>
-                <button id="modalButton3" style="width: 100%;" onclick="showGraphicByUser()" type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#modalByUser">
-                    <i class="far fa-chart-bar"></i>
-                    Ver tiempo promedio por día
-                </button>
-            </div> 
-            
-            <div id="loading" style="display:none; width: 100%;">
-                <span>Generando...</span>
-            </div>
-
-
-            <script src='../blocks/promuser/main/main.js'></script>
-        EOT;
-        //--------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+        $contentPromUser = str_replace('%selectOptions%',$selectOptions,$contentPromUser);
 
         //Se retorna el contenido declarado para el bloque de PromUser
         $this->content->text = $contentPromUser;
